8dd95099f939621dfbe6cb5065023192f97a0d3f
zms@innovacia.ru <zms@innovacia.ru>
2019-04-19 18:25:21 +0700

    Update PyCharm project

--- first_test.py
+++ first_test.py
@@ -20,6 +20,7 @@ class first_test(unittest.TestCase):
         wd.get("http://localhost/adressbook")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -32,6 +33,7 @@ class first_test(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -46,6 +48,7 @@ class first_test(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_goups_page(wd)
 
     def return_to_goups_page(self, wd):
         wd.find_element_by_link_text("group_page").click()
@@ -55,20 +58,14 @@ class first_test(unittest.TestCase):
 
     def test_first_test(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="31244", header="3123123", footer="3123123"))
-        self.return_to_goups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):     # Добавление пустой группы
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_goups_page(wd)
         self.logout(wd)
 
     def tearDown(self):
